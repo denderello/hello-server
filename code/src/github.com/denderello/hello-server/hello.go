2cba21e229623f0c58aef6bd11215f2d8f308d91
Dennis Benkert <spinecrasher@googlemail.com>
2014-05-24 18:19:20 +0000

    added request parameters to greeting message

--- code/src/github.com/denderello/hello-server/hello.go
+++ code/src/github.com/denderello/hello-server/hello.go
@@ -6,7 +6,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi there")
+		fmt.Fprintf(w, "Hi %s! Nice to see you!", r.URL.Path[1:])
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
