e8f8171fd175f15db75d0daffdcedd568a8ab5f7
Dennis Benkert <spinecrasher@googlemail.com>
2014-05-27 16:56:16 +0000

    updated build file to use hardlinks instead of copying files

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-rm -rf build/{src,pkg,bin}
+# create the needed GOPATH structure so that gotools plays nicely
 mkdir -p build/{src/github.com/denderello/hello-server,pkg,bin}
 
-cp *.go build/src/github.com/denderello/hello-server
+# link all needed projects files to intermediate GOPATH
+# build the final container with the compiled binary
+(cd build/src/github.com/denderello/hello-server ; ln -f ../../../../../*.go)
+(cd build ; ln -f ../Makefile)
 
+# build the development Docker container
 (cd build ; ./build.sh)
 
+# build and install the project to the intermediate GOPATH
 ./bin/go.sh install github.com/denderello/hello-server
 
+# build the final container with the compiled binary
 docker build -t denderello/hello-server .
