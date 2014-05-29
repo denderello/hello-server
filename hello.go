b22d87e58d2ad11727d9f04a62d1f1744b283808
Dennis Benkert <spinecrasher@googlemail.com>
2014-05-29 15:31:38 +0000

    added http method restriction to homepage route

--- hello.go
+++ hello.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/{names}", HomeHandler)
+	router.HandleFunc("/{names}", HomeHandler).
+		Methods("GET")
 
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":8000", nil))
