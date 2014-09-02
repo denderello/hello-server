2ee34053cc19d95375c8fc6096af6003283ff590
Dennis Benkert <spinecrasher@googlemail.com>
2014-09-02 18:04:59 +0000

    added helper methods to HomeHandler

--- handlers.go
+++ handlers.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
+
 	"github.com/gorilla/mux"
 )
 
 func HomeHandler(response http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
-	names := vars["names"]
+	names := splitNamesParameter(string(vars["names"]))
 
-	fmt.Fprintf(response, "Hi %s! Nice to see you!", names)
+	fmt.Fprintf(response, "Hi %s! Nice to see you!", concatNames(names))
+}
+
+func splitNamesParameter(concatedNames string) []string {
+	return strings.Split(concatedNames, " ")
+}
+
+func concatNames(names []string) string {
+	return strings.Join(names, ", ")
 }
